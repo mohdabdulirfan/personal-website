551204a9efa660fbeb6c7bec596c531631c3bb96
mohdabdulirfan <55888027+mohdabdulirfan@users.noreply.github.com>
2020-06-23 13:15:51 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const app = express()
 app.use(express.static("public"))
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+// app.get("/", function (req, res) {
+//   res.send("<h1>Hello World!</h1>")
+// })
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
